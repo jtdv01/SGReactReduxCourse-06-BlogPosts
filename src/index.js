3d034662c299bcd8367fd3205260e138039e0dbd
jet <jtdv01@gmail.com>
2016-12-12 23:44:03 +1100

    Added routes

--- src/index.js
+++ src/index.js
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+// Middlewares
+import promiseMiddleware from 'redux-promise';
+import { composeWithDevTools } from 'remote-redux-devtools';
+const composeEnhancers = composeWithDevTools({realtime: true});
+const store = createStore(reducers,composeEnhancers(
+  applyMiddleware(promiseMiddleware)
+));
+
+// React router
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes}/>
   </Provider>
   , document.querySelector('.container'));
